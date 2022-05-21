a9e4412b8f0068f634973b623a5e28149bb04014
rashidul191 <rashidul.191cse.gub@gmail.com>
2022-05-21 16:14:10 +0600

    chose 1 for me done.

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,23 +1,17 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
-import './Cart.css'
+import "./Cart.css";
 
 const Cart = (props) => {
-  
-  const {product} = props;
-  const {name, img} = product
-
-//   console.log(props)
-//   let fourProduct = [];
-
-
-
+  const { name, img } = props.product;
   return (
     <div className="my-4 d-flex justify-content-between">
       <img className="product-img" src={img} alt="" />
       <h6>{name}</h6>
-      <button className="btn btn-danger" > <FontAwesomeIcon icon={faTrashCan}></FontAwesomeIcon> </button>
+      <button className="btn btn-danger">
+        <FontAwesomeIcon icon={faTrashCan}></FontAwesomeIcon>{" "}
+      </button>
     </div>
   );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -3,24 +3,41 @@ import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
 
 const Shop = () => {
+  // set product , cart and singleProductName
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState([]);
+  const [singleProductName, setSingleProductName] = useState([]);
 
-  // console.log(products)
-
+  // load data public folder
   useEffect(() => {
     fetch("product.json")
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
 
-
+  // handle Add to cart product
   const handleAddToCart = (product) => {
     let newProductAddCart = [];
     newProductAddCart = [...cart, product];
     setCart(newProductAddCart);
   };
-//   console.log(cart)
+
+  // generator random number and find out choose any product
+  const handleChooseOneForMe = () => {
+    const randomNumber = parseInt(Math.random() * cart.length);
+    if (randomNumber === 0) {
+      return handleChooseOneForMe();
+    } else {
+      const singleProduct = cart[randomNumber];
+      setSingleProductName(singleProduct);
+    }
+  };
+
+  // Buy Now Product btn
+  const buyNowProduct = () => {
+    alert("congratulations");
+  };
+
   return (
     <div className="container">
       <div className="row">
@@ -35,12 +52,64 @@ const Shop = () => {
         </div>
         <div className="col col-2 col-md-3 ms-2 bg-info cart-container">
           <h2>Order Summary</h2>
-        
-           {
-               cart.map((product)=>  <Cart product = {product} key= {product.id} ></Cart>)
-           }
-          
-          <button className="btn btn-light btn-lg border-secondary">Choose 1 for me</button>
+
+          {cart.map((product) => (
+            <Cart product={product} key={product.id}></Cart>
+          ))}
+
+          {/* <!-- Modal --> */}
+          <div
+            class="modal fade"
+            id="exampleModal"
+            tabindex="-1"
+            aria-labelledby="exampleModalLabel"
+            aria-hidden="true"
+          >
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">
+                    Congratulations!!! Product For Me
+                  </h5>
+                  <button
+                    type="button"
+                    class="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                  ></button>
+                </div>
+                <div class="modal-body">
+                  <img className="w-25" src={singleProductName.img} alt="" />
+                  <h5>{singleProductName.name}</h5>
+                  <p>Price: ${singleProductName.price}</p>
+                </div>
+                <div class="modal-footer">
+                  <button
+                    type="button"
+                    class="btn btn-secondary"
+                    data-bs-dismiss="modal"
+                  >
+                    Don't Like
+                  </button>
+                  <button
+                    onClick={buyNowProduct}
+                    type="button"
+                    class="btn btn-primary"
+                  >
+                    Buy Now
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <button
+            onClick={handleChooseOneForMe}
+            className="btn btn-light btn-lg border-secondary"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+          >
+            Choose 1 for me
+          </button>
         </div>
       </div>
     </div>
