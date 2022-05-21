a58c73bb184e2d971b542188561fb87926c49413
rashidul191 <rashidul.191cse.gub@gmail.com>
2022-05-21 11:48:22 +0600

    navbar section done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React from "react";
 
 const Header = () => {
-    return (
-        <div>
-            
+  return (
+    <div>
+      <nav class=" navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+          <a class="navbar-brand" href="/home">
+            Easy-Tech
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+              <li class="nav-item mx-4">
+                <a class="nav-link" href="/home">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item mx-4">
+                <a class="nav-link" href="/about">
+                  About
+                </a>
+              </li>
+              <li class="nav-item mx-4">
+                <a class="nav-link" href="/order">
+                  Order
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-    );
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
